d30479a29bb03692b9cb773bb5f333b515796e47
silatjedi <silatjedi@gmail.com>
2016-03-04 10:56:00 -0800

    renamed a few files

--- app/src/main/java/com/silatsakti/tokimessenger/MainActivity.java
+++ app/src/main/java/com/silatsakti/tokimessenger/MainActivity.java
@@ -4,16 +4,11 @@ import android.app.Activity;
 import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.Editable;
-import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
@@ -33,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         // Create the Keyboard
-        keyboard = new Keyboard(this,R.xml.latinkb);
+        keyboard = new Keyboard(this,R.xml.latinKeyBoard);
 
         // Lookup the KeyboardView
         keyboardView= (KeyboardView)findViewById(R.id.keyboardview);
